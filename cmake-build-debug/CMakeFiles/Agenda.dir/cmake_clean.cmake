183ccf4e8a7bb035e5648d7c61a20033d2ea31e8
Ccasero2021 <126870039+CristobalCasero@users.noreply.github.com>
2024-04-18 17:30:04 +0200

    first

---
+++ cmake-build-debug/CMakeFiles/Agenda.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Agenda.exe"
+  "Agenda.exe.manifest"
+  "Agenda.pdb"
+  "CMakeFiles/Agenda.dir/main.c.obj"
+  "libAgenda.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Agenda.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
